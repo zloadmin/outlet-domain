fffa049915aaef7662670a753ada190420728e78
Vladimir Karpovich <dev.karpovich@gmail.com>
2020-07-28 17:27:16 +0300

    fix

--- src/Providers/OutletDomainServiceProvider.php
+++ src/Providers/OutletDomainServiceProvider.php
@@ -2,9 +2,21 @@
 declare(strict_types=1);
 namespace LaravelOutletDomain\Providers;
 use Illuminate\Support\ServiceProvider;
+use LaravelOutletDomain\OutletDomainFactory;
 
 class OutletDomainServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('outlet-domain', function () {
+            return new OutletDomainFactory();
+        });
+    }
     /**
      * Bootstrap any application services.
      *
@@ -16,4 +28,5 @@ class OutletDomainServiceProvider extends ServiceProvider
             __DIR__.'/../../config/outlet-domain.php' => config_path('outlet-domain.php'),
         ]);
     }
+
 }
